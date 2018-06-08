fc3aca622cb3ca87e7203d41115d9a47ab190459
Poga Po <poga.bahamut@gmail.com>
2018-06-09 01:19:21 +0800

    fix runner test

--- test/runner.js
+++ test/runner.js
@@ -5,13 +5,13 @@ const request = require('request-promise')
 tape('http', async function (t) {
   const app = new Runner()
 
-  app.get('/foo', async function fooHandler (ctx) {
+  app.get('/foo', async function (ctx) {
     ctx.body = 'bar'
   })
 
   let srv = app.run()
-  let ret = await request.get('http://localhost:3000/foo')
-  t.same(ret, 'bar')
+  let ret = await request.post('http://localhost:3000/GET-foo')
+  t.same(ret, '{"body":"bar","headers":{"vary":"Origin","content-type":"text/plain; charset=utf-8","content-length":"3"},"statusCode":200}')
   srv.close()
   t.end()
 })
@@ -19,13 +19,13 @@ tape('http', async function (t) {
 tape('event', async function (t) {
   const app = new Runner()
 
-  app.on('foo', async function fooHandler (ctx) {
+  app.on('foo', async function (ctx) {
     ctx.body = 'bar'
   })
 
   let srv = app.run()
-  let ret = await request.post('http://localhost:3000/fooHandler')
-  t.same(ret, 'bar')
+  let ret = await request.post('http://localhost:3000/EVENT-foo')
+  t.same(ret, '{"body":"bar","headers":{"vary":"Origin","content-type":"text/plain; charset=utf-8","content-length":"3"},"statusCode":200}')
   srv.close()
   t.end()
 })
