f1cda54933f8226fb5620fd1f598d8d79d8d6d94
Poga Po <poga.bahamut@gmail.com>
2018-06-15 01:12:20 +0800

    remove unused file

--- assert.js
+++
@@ -1,7 +0,0 @@
-function assertNotAnonymous (name) {
-  if (name === 'anonymous' || name === '') {
-    throw new Error('Anonymous function is not allowed. Please specify a name')
-  }
-}
-
-module.exports = { assertNotAnonymous }
